8d42b30c6cc6f066011ee376d61485ac731bcda8
Carlos Erasmo Téllez Espejel <a01274912@sldid17-2.local>
2019-05-13 09:02:53 -0500

    Listo

--- app/controllers/cars/create.js
+++ app/controllers/cars/create.js
@@ -1,4 +1,19 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class CarsCreateController extends Controller {
+	@action
+	createCar(){
+		let car = this.store.createRecord('car', {
+			brand: this.brand,
+			color: this.color,
+			price: this.price,
+			wheels: this.wheels,
+			img_url: this.img_url
+		});
+		car.save();
+		alert('Registro Creado');
+		
+	}
 }
